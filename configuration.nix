ab9c5c4667f3e5462e2961ee9ec7f4b3f4caf179
Rahinur Bin Naushad <rahinur2004@gmail.com>
2026-04-12 16:17:26 +0600

    added lazygit
    commiting through lazygit already

--- configuration.nix
+++ configuration.nix
@@ -127,6 +127,7 @@
   # List packages installed in system profile. To search, run:
   # $ nix search wget
   environment.systemPackages = with pkgs; [
+    lazygit
     vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
     wget
     gcc
