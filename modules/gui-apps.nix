37ddb3b551297ed19bd05e7acd491f09c8289d13
Rahinur Bin Naushad <rahinur2004@gmail.com>
2026-04-16 15:00:37 +0600

    added mpv

--- modules/gui-apps.nix
+++ modules/gui-apps.nix
@@ -7,6 +7,7 @@
   programs.kdeconnect.enable = true;
 
   environment.systemPackages = with pkgs; [
+    mpv
     bitwarden-desktop
     inputs.zen-browser.packages.${pkgs.stdenv.hostPlatform.system}.default
     gnome-tweaks
