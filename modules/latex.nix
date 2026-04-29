5c9e2daf208ef2716b2d2fe11e2567d3aea221d2
Rahinur Bin Naushad <rahinur2004@gmail.com>
2026-04-29 14:11:52 +0600

    nothngs is installing

--- modules/latex.nix
+++ modules/latex.nix
@@ -1,17 +1,13 @@
-{pkgs, inputs, ... }: 
+{config, pkgs, inputs, ... }: 
 
 {
   environment.systemPackages = with pkgs; [
     (texlive.combine {
       inherit (texlive)
-        scheme-medium   # Essential base
-        latexmk         # Required for Neovim/VimTeX automation
-        biber           # Modern bibliography backend
-        biblatex        # Better citation management
-        amsmath         # Non-negotiable for CS/Math formulas
-        hyperref        # Makes your PDF links/ToC clickable
-        geometry        # Easiest way to set margins (usually required by Uni)
-        enumitem;       # Better control over bullet points/lists
+        scheme-small
+        latexmk    
+        biber     
+        biblatex;
     })
   ];
 }
