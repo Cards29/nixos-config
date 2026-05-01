31734224072bcf7f5de92527e5e153cb6089cf55
Rahinur Bin Naushad <rahinur2004@gmail.com>
2026-05-02 00:40:25 +0600

    caffeine added to gnome

--- modules/gnome-extensions.nix
+++ modules/gnome-extensions.nix
@@ -2,6 +2,7 @@
 {
   environment.systemPackages = with pkgs; [
     gnomeExtensions.all-in-one-clipboard
+    gnomeExtensions.caffeine
     gnomeExtensions.blur-my-shell
     gnomeExtensions.tweaks-in-system-menu
   ];
